60249177558b50ba5f440114482e72cb52411cec
Siro1998 <43830622+Siro1998@users.noreply.github.com>
2021-10-13 19:12:44 -0400

    wk6

---
+++ wk5/script.js
@@ -0,0 +1,26 @@
+console.log('hi tweeter')
+
+// import TwitterApi from 'twitter-api-v2'
+const { TwitterApi } = require('twitter-api-v2');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.NAME);
+
+console.log(TwitterApi);
+
+const client = new TwitterApi({
+    appKey: process.env.AppKey,
+    appSecret: process.env.AppSecret,
+    accessToken: process.env.AccessToken,
+    accessSecret: process.env.AccessSecret,
+  });
+
+tweet();
+setInterval(tweet,2000);
+
+async function tweet(){
+    const response = await client.v1.tweet('yoyoyoyo');
+    console.log(response.created_at);
+}
